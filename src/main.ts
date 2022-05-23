faec17215fae30cc54b6aecd3e383d494643ace4
Sajadmhy <sajad.mahyaei@gmail.com>
2022-05-23 14:20:51 -0400

    Add PORT to main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,10 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
 
 async function bootstrap() {
+
+  const PORT = process.env.PORT || 5000;
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  await app.listen(5000);
+  await app.listen(PORT);
 }
 bootstrap();
